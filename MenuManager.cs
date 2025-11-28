ae31a8e531128c5217e92c1dcc61ca0d77469644
nonberw <nejm873@gmail.com>
2025-11-28 18:44:10 +0500

    Add files via upload

---
+++ MenuManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("GameScene"); // Замените на имя вашей сцены
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game Quit"); // Работает только в билде
+    }
+}
